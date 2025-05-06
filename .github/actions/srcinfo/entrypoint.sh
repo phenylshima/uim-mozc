214af10230a7c17bbe177b5edbd911c6f70f98a8
femshima <49227365+femshima@users.noreply.github.com>
2025-05-06 15:19:13 +0900

    remove permission display

--- .github/actions/srcinfo/entrypoint.sh
+++ .github/actions/srcinfo/entrypoint.sh
@@ -5,7 +5,6 @@ WORKPATH=$GITHUB_WORKSPACE/$INPUT_PKGNAME
 cd $WORKPATH
 
 echo "::group::Generating new .SRCINFO based on PKGBUILD"
-ls -la
 makepkg --printsrcinfo > .SRCINFO
 git diff .SRCINFO
 echo "::endgroup::"
