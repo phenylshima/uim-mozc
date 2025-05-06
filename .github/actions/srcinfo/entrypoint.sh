b3b8bc497f8a2cf7f5b4d3266b0182e2e7853048
femshima <49227365+femshima@users.noreply.github.com>
2025-05-06 14:46:40 +0900

    fix permission issues

--- .github/actions/srcinfo/entrypoint.sh
+++ .github/actions/srcinfo/entrypoint.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 WORKPATH=$GITHUB_WORKSPACE/$INPUT_PKGNAME
 cd $WORKPATH
+sudo chown -R builder:builder $WORKPATH/
 
 echo "::group::Generating new .SRCINFO based on PKGBUILD"
 makepkg --printsrcinfo > .SRCINFO
