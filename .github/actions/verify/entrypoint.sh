7ee398c9e8994b98ef437a37e0415de3b9837d8b
phenylshima <49227365+phenylshima@users.noreply.github.com>
2025-05-06 17:04:01 +0900

    split verify and srcinfo (#54)
    * split verify and srcinfo
    
    * fix Dockerfile
    
    * same workflow, different jobs
    
    * remove verify.yml
    
    * fix permission issues
    
    * add pkgbuild cond
    
    * update PKGBUILD
    
    * fix permission issue
    
    * reinstate USER statement
    
    * builder user
    
    * see permission
    
    * specify user id 1001 for runner
    
    * remove permission display
    
    * slimer verify
    
    * use new commit
    
    * use cache
    
    * Apply automatic changes
    
    * remove ls
    
    ---------
    
    Co-authored-by: femshima <49227365+femshima@users.noreply.github.com>

---
+++ .github/actions/verify/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "::group::Updating"
+sudo pacman -Syu --noconfirm
+echo "::endgroup::"
+
+WORKPATH=$GITHUB_WORKSPACE/$INPUT_PKGNAME
+cd $WORKPATH
+
+echo "::group::Linking cache directory"
+mkdir -p "$GITHUB_WORKSPACE"/.cache
+ln -s "$GITHUB_WORKSPACE"/.cache "$HOME"/.cache
+echo "::endgroup::"
+
+echo "::group::Updating archlinux-keyring"
+sudo pacman -S --noconfirm archlinux-keyring
+echo "::endgroup::"
+
+echo "::group::Installing depends using paru"
+source PKGBUILD
+paru -Syu --removemake --needed --noconfirm --mflags=CFLAGS="-Wno-implicit-function-declaration" "${depends[@]}" "${makedepends[@]}" 
+echo "::endgroup::"
+
+echo "::group::Running makepkg"
+makepkg
+echo "::endgroup::"
