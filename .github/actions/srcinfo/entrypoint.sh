3ef4e8ebd3e92838a3f1f252a36d589f5385fc2e
femshima <49227365+femshima@users.noreply.github.com>
2025-05-06 15:11:10 +0900

    see permission

--- .github/actions/srcinfo/entrypoint.sh
+++ .github/actions/srcinfo/entrypoint.sh
@@ -5,6 +5,7 @@ WORKPATH=$GITHUB_WORKSPACE/$INPUT_PKGNAME
 cd $WORKPATH
 
 echo "::group::Generating new .SRCINFO based on PKGBUILD"
+ls -la
 makepkg --printsrcinfo > .SRCINFO
 git diff .SRCINFO
 echo "::endgroup::"
