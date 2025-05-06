06680a1d3a4071a20d801bdd181793ad6d3ade93
femshima <49227365+femshima@users.noreply.github.com>
2025-05-06 15:22:00 +0900

    slimer verify

--- .github/actions/verify/entrypoint.sh
+++ .github/actions/verify/entrypoint.sh
@@ -5,32 +5,11 @@ echo "::group::Updating"
 sudo pacman -Syu --noconfirm
 echo "::endgroup::"
 
-# Set path
 WORKPATH=$GITHUB_WORKSPACE/$INPUT_PKGNAME
-HOME=/home/builder
-echo "::group::Copying files from $WORKPATH to $HOME/gh-action"
-# Set path permision
-cd $HOME
-mkdir gh-action
-cd gh-action
-cp -rfv "$GITHUB_WORKSPACE"/.git ./
-cp -fv "$WORKPATH"/* ./
-echo "::endgroup::"
-
-echo "::group::Linking cache directory"
-sudo mkdir -p "$GITHUB_WORKSPACE"/.cache
-sudo chown -R builder:builder "$GITHUB_WORKSPACE"/.cache
-sudo chmod -R 775 "$GITHUB_WORKSPACE"/.cache
-ln -s "$GITHUB_WORKSPACE"/.cache "$HOME"/.cache
-echo "::endgroup::"
+cd $WORKPATH
 
 echo "::group::Updating archlinux-keyring"
 sudo pacman -S --noconfirm archlinux-keyring
-echo "::endgroup::"
-
-echo "::group::PKGBUILD and .SRCINFO diff"
-git diff PKGBUILD
-git diff .SRCINFO
 echo "::endgroup::"
 
 echo "::group::Installing depends using paru"
@@ -40,10 +19,4 @@ echo "::endgroup::"
 
 echo "::group::Running makepkg"
 makepkg
-echo "::endgroup::"
-
-echo "::group::Copying files from $HOME/gh-action to $WORKPATH"
-sudo cp -fv PKGBUILD "$WORKPATH"/PKGBUILD
-sudo cp -fv .SRCINFO "$WORKPATH"/.SRCINFO
-sudo chmod -R 775 "$GITHUB_WORKSPACE"/.cache
 echo "::endgroup::"
